8cdb1d6d30f94aa6b13535554ff63a0613dd073d
Ian Ge <iange2000@outlook.com>
2022-03-08 21:48:06 +0800

    Step 1 plus some fixes

--- main.m
+++ main.m
@@ -23,12 +23,53 @@
 ## Created: 2022-03-04
 
 function retval = main
-    
+    ## Step 1
+    G_n = 5;        # number of particles
+    N = 10;         # number of way-points
+    startpoint = [0; 0];
+    endpoint = [30; 30];
+    position_range = [min([startpoint, endpoint](2,:)), max([startpoint, endpoint](2,:))];
+    xIntervals = linspace(startpoint(1),endpoint(1),N);
+    # swarm initialization
+    swarm = rand(G_n, N)*10 - 5 + linspace(startpoint(2),endpoint(2),N);
+    swarm(:,1) = startpoint(2);
+    swarm(:,end) = endpoint(2);
+    swarm = swarm - (swarm < position_range(1)) .* swarm - (swarm > position_range(2)) .* swarm + (swarm > position_range(2))*position_range(2);
+    plot(xIntervals,swarm(1,:), "-*")
+    # constraints
+    theta_Tmax = 60;
+    theta_Cmax = 45;
+    % threat_source = [6.2280, 17.781, 15.681, 6.5280, 22.581, 15.057, 21.036; 8.5230, 4.6080, 17.208, 13.629, 21.108, 11.835, 15.846; 2.2826, 1.9663, 2.8540, 2.0762, 1.9393, 2.4483, 2.4404];
+    threat_source = [30; 0; 0];
+
+    # PSO parameters
+    maxgeneration = 1;
+    omega = linspace(0.7, 0.4, maxgeneration);
+    phi_p = 0.2;    # cognitive coefficient
+    phi_g = 0.2;    # social coefficient
+    velocity_range = [-6.0, 6.0];
+    # objective function weights
+    omega_d = 0.3;
+    omega_c = 0.5;
+    CT = 4.5;
+    N_W = N;
+
+    ## Step 2
+    generation = 1;
+    while generation <= maxgeneration
+        objective = zeros(G_n, 1);
+        for i = 1:1
+            objective(i) = F(swarm(i,:), xIntervals, threat_source, theta_Tmax, theta_Cmax, omega_d, omega_c, CT, N_W);
+        endfor
+        objective
+        generation++;
+    endwhile
+
 endfunction
 
 function d = distance (varargin)
 ## Calculate a bunch of different distance using function overloading
-    [data, opt] = parseparams(varargin)
+    [data, opt] = parseparams(varargin);
     if columns(data) == 1
     ## input format: X = [X1, X2, ...]
         X = data{:};
@@ -111,23 +152,23 @@ function d = distance (varargin)
     endif
 endfunction
 
-function output = cos (X, varargin)
-## overloads the built-in cos function to accept the length of the triangle and calculate the cosin of that angle
-    if columns(varargin) == 0
-        output = cos(X);
-    elseif columns(varargin) == 2
-        pending = (X^2 + varargin{1}^2 - varargin{2}^2)/(2*X*varargin{1});
-        if pending >= -1 && pending <= 1
-            output = pending;
-        else
-            error("Input sides do not form a triangle.")
-        endif
-    endif
-endfunction
+% function output = cos (X, varargin)
+% ## overloads the built-in cos function to accept the length of the triangle and calculate the cosin of that angle
+%     if columns(varargin) == 0
+%         output = cos(X);
+%     elseif columns(varargin) == 2
+%         pending = (X^2 + varargin{1}^2 - varargin{2}^2)/(2*X*varargin{1});
+%         if pending >= -1 && pending <= 1
+%             output = pending;
+%         else
+%             error("Input sides do not form a triangle.")
+%         endif
+%     endif
+% endfunction
 
 function trajectory = particle2Trajectory (particle, xIntervals)
 ## convert particle into trajectory
-    trajectory = [xIntervals; particle, altitude([xIntervals; particle]')'];
+    trajectory = [xIntervals; particle; altitude([xIntervals; particle])];
 endfunction
 
 function pathDistance = F_d (varargin)
@@ -161,7 +202,7 @@ endfunction
 
 function bool = threatConflict (course, threat_source)
 ## input format: course = [P1, P2, ...] = [x1, x2, ...; y1, y2, ...](2 by N), threat_source = [T1, T2, ...; r1, r2, ...] = [x1, x2, ...; y1, y2, ...; r1, r2, ...](3 by T)
-## output format: bool (1 by 1) (if the course conflicts with any threar_source)
+## output format: bool (1 by 1) (if the course conflicts with any threat_source)
     d1 = distance(course, threat_source(1:2,:), 'recursive');   ## distance from each threat_source to each slice       ## (T by N-1)
     d2 = distance(course, threat_source(1:2,:), 'mesh');        ## distance from each threat_source to each way-point   ## (N by T)
     d3 = distance(course);                                      ## length of each slice                                 ## (1 by N-1)
@@ -171,8 +212,18 @@ function bool = threatConflict (course, threat_source)
     booltable2 = d1>threat_source(3,:)';        ## if the distance from each threat_source to each slice is greater than the source's radius            ## (T by N-1)
     booltable3 = d3.^2>abs(diff(d2.^2))';       ## if c^2>|a^2-b^2|                                                                                     ## (T by N-1)
 
-    booltable = ~(~booltable1 & booltable2 & ~booltable3);      ## if each slice conflicts with each threat_source       ## (T by N-1)
+    booltable = booltable1 | ~booltable1 & ~booltable2 & booltable3;## if each slice conflicts with each threat_source   ## (T by N-1)
     bool = sum(sum(booltable,"native"),"native");               ## if any slice conflicts with any threat_source         ## (1 by 1)
+
+    disp("threatConflict")
+    d1
+    d2
+    d3    
+    booltable1
+    booltable2
+    booltable3
+    booltable
+    bool
 endfunction
 
 function theta = turningAngle (segment)
@@ -188,7 +239,10 @@ function bool = maxTurningAngle (trajectory, theta_Tmax)
     for i = 1:columns(trajectory)-2
         bool(i) = turningAngle(trajectory(:,i:i+2)) < theta_Tmax;
     endfor
-endfunciton
+
+    disp("maxTurningAngle")
+    bool
+endfunction
 
 function cooperativeFactor = F_c (particle, CT)
 ## input format: particle = [y1, y2, ..., yN]
@@ -217,7 +271,7 @@ endfunction
 function theta = climbingDivingAngle (slice)
 ## input format: slice = [x1, x2;y1, y2]
     theta = atand(abs(diff(altitude(slice)))/sqrt(sum(diff(slice,2).^2)));
-endfunciton
+endfunction
 
 function bool = maxClimbingDivingAngle (course, theta_Cmax)
 ## input format: course = [P1, P2, ...] = [x1, x2, ...; y1, y2, ...](2 by N), theta_Cmax is in degree
@@ -226,13 +280,16 @@ function bool = maxClimbingDivingAngle (course, theta_Cmax)
     for i = 1:columns(course)-1
         bool(i) = climbingDivingAngle(course(:,i:i+1)) < theta_Cmax;
     endfor
+
+    disp("maxClimbingDivingAngle")
+    bool
 endfunction
 
 function objective = F (particle, xIntervals, threat_source, theta_Tmax, theta_Cmax, omega_d, omega_c, CT, N_W)
 ## input format: particle = [y1, y2, ..., yN]; xIntervals = [x1, x2, ..., xN]
 ## output format: objective is a real number
     trajectory = particle2Trajectory (particle, xIntervals);
-    if threatConflict([xIntervals; particle]) || ~prod(maxTurningAngle(trajectory, theta_Tmax), "native") || ~prod(maxClimbingDivingAngle(trajectory(1:2,:), theta_Cmax), "native")
+    if threatConflict([xIntervals; particle], threat_source) || ~prod(maxTurningAngle(trajectory, theta_Tmax), "native") || ~prod(maxClimbingDivingAngle(trajectory(1:2,:), theta_Cmax), "native")
         objective = inf;
     else
         objective = omega_d * F_d(trajectory) + omega_c * F_c(particle, CT) + (1-omega_c-omega_d) * F_a(trajectory, N_W);
