19fbea89ba10c2ed8b78e40c913759010caa8cca
Ian Ge <iange2000@outlook.com>
2022-04-26 22:56:39 +0800

    C++ simulation project starts; Folder architecture refreshed

---
+++ CXX_simulation/CXX_simulation.cpp
@@ -0,0 +1,26 @@
+﻿// CXX_simulation.cpp : Defines the entry point for the application.
+//
+
+#include "CXX_simulation.h"
+
+//using namespace std;
+
+using namespace Eigen;
+
+//using Eigen::ArrayXXd;
+//using Eigen::MatrixXd;
+//using Eigen::seq;
+//using Eigen::placeholders::all;
+
+int main()
+{
+	MatrixXd m = MatrixXd::Random(4, 4);
+	//MatrixXd n(3, 3);
+	//MatrixXd r1(4, 4);
+	//MatrixXd r2(3, 3);
+	//m << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+	//n << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+	//r1 = m(seq(2, placeholders::last));
+	//r2 = r1 * r1;
+	std::cout << m;
+}
